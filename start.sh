4ae53f1defb99de63653019f9c821a11ad6886dc
Fernando Zaragoza <fer.zm@outlook.com>
2020-09-02 10:53:28 -0500

    Modifying some utility scripts.

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 source /opt/ros/melodic/setup.bash
 source /catkin_ws/devel/setup.bash
 export DISPLAY=192.168.1.100:0.0
-export LIBGL_ALWAYS_INDIRECT=0
 export TURTLEBOT3_MODEL=burger
 # roslaunch f1tenth_gym_ros gym_bridge.launch
 # catkin_make && source /catkin_ws/devel/setup.bash 
---
+++ vsterminal.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec -it interesting_hermann bash
